b70dad472c9594ae9e7a2da577d9e9c27acf768c
Matheus Centa <matheuscenta@gmail.com>
2020-09-18 21:01:22 +0200

    Improve names in the ResNet classes

--- vq-vae/resnet.py
+++ vq-vae/resnet.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 
 
 class ResConvBlock(tf.keras.Model):
-    def __init__(self, n_in, n_residual_hidden):
+    def __init__(self, n_input_filters, n_residual_filters):
         super(self, ResConvBlock).__init__()
         self.block = tf.keras.models.Sequential([
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2D(n_residual_hidden, 3, strides=1,
+            tf.keras.layers.Conv2D(n_residual_filters, 3, strides=1,
                 padding='same'),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2D(n_in, 1, strides=1,
+            tf.keras.layers.Conv2D(n_input_filters, 1, strides=1,
                 padding='valid'),
         ])
 
@@ -18,19 +18,16 @@ class ResConvBlock(tf.keras.Model):
 
 
 class ResidualStack(tf.keras.Model):
-    def __init__(self, n_in, n_residual_hidden, n_residual_blocks,
-        apply_relu=False):
+    def __init__(self, n_input_filters, n_residual_filters, n_residual_blocks):
         super(self, ResidualStack).__init__()
-        self._n_in = n_in
-        self._n_residual_hidden = n_residual_hidden
+        self._n_input_filters = n_input_filters
+        self._n_residual_filters = n_residual_filters
         self._n_residual_blocks = n_residual_blocks
-        self.apply_relu = apply_relu
 
         self.stack = tf.keras.models.Sequential()
         for _ in range(n_residual_blocks):
-            self.stack.add(ResConvBlock(n_in, n_residual_hidden))
-        if apply_relu:
-            self.stack.add(tf.keras.layers.ReLU())
+            self.stack.add(ResConvBlock(n_input_filters, n_residual_filters))
+        self.stack.add(tf.keras.layers.ReLU())
 
     def call(self, x):
         return self.stack(x)
